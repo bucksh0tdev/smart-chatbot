1a471bcf75f2572446334a0df4b40f364c8c7417
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-02-05 14:35:12 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const x = require("./mains.js");
-const xs = new x.Client("sample-api", "Bot Name");
-
-   xs.chat({
-    message: "Hello",
-    user: "1",
-    language: "tr"
-  }).then(xx => console.log)
