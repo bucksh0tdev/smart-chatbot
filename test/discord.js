e9972b96fda396745638c67d394566c54c81e77e
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-02-05 14:09:53 +0300

    Create discord.js

---
+++ test/discord.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL"],
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+  ]
+});
+const chatbot = require("smart-chatbot"); 
+const chatclient = new chatbot.Client("SECRET KEY", "BOT NAME");
+
+client.on("message", async(message) => {
+  if(!message || !message.content || !message.author || message.author.bot) return;
+  
+  if (message.content.startsWith(`<@${client.user.id}>`) || message.content.startsWith(`<@!${client.user.id}>`)) {
+       let args = message.content.split(" ").slice(1);
+       let soru = args.slice(0).join(' ');
+
+       let reply = await chatclient.chat({ 
+           message: soru, 
+           user: message.author.id 
+        });
+     message.channel.send(reply);
+  }
+});
+
+client.login("SECRET DISCORD BOT TOKEN");
