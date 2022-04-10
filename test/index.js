07aa8a33e55c994850a89ecdf2ce8b4d62aea558
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-04-10 12:31:48 +0300

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 const chatbot = require("smart-chatbot");
-const chatclient = new chatbot.Client("SECRET KEY", "BOT NAME");
+const chatclient = new chatbot.Client({ secretkey: "SECRET KEY", botname: "BOT NAME", ownername: "OWNER NAME" });
 
 chatclient.chat({
   message: "Hello",
   user: "SECRET USER ID",
   language: "tr" // (tr, en)
-}).then(answer => console.log);
+}).then(answer => console.log(answer));
