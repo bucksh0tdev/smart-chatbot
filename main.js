2ad18b822efd366b2efb88b55905fef07b0ac5f3
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-04-10 12:31:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,21 +4,30 @@ const version = require('./package.json').version;
 const packagename = require('./package.json').name;
 
 class Client {
-    constructor(secret, botname, logging = false) {
+    constructor(mainjson) {
 
+    let logging;
+    if(!mainjson.logging || mainjson.logging != true) {
+        logging = false;
+    } else {
+        logging = true;
+    }
     if(logging == true)
     console.log(`[${packagename}] Enabling..`);
         
         this.chat = function(json) {
             return new Promise(async(resolve, reject) => {
-
+                if(!mainjson.secretkey || typeof mainjson.secretkey !== "string") return console.error("[" + packagename + "] Secret Key Problem");
+                if(!mainjson.botname || typeof mainjson.secretkey !== "string") return console.error("[" + packagename + "] Bot Name Problem");
+                if(!mainjson.ownername || typeof mainjson.secretkey !== "string") return console.error("[" + packagename + "] Owner Name Problem");
+                
                 if(!json.message || typeof json.message !== "string") reject("No message was provided");
                 if(!json.user || typeof json.user !== "number") json.user = 1;
-                if(!json.language || typeof json.language !== "string" || (json.language != "tr" && json.language != "en")) json.language = "en";
+                if(!json.language || typeof json.language !== "string") json.language = "en";
 
-                axios.get(`${baseURL}/chatbot?message=${encodeURIComponent(json.message)}&user=${encodeURIComponent(json.user)}&language=${encodeURIComponent(json.language)}&botname=${encodeURIComponent(botname)}`, {
+                axios.get(`${baseURL}/chatbot?message=${encodeURIComponent(json.message)}&user=${encodeURIComponent(json.user)}&language=${encodeURIComponent(json.language)}&botname=${encodeURIComponent(mainjson.botname)}&ownername=${encodeURIComponent(mainjson.ownername)}`, {
                     headers: {
-                        'Authorization': `${secret}`,
+                        'Authorization': `${mainjson.secretkey}`,
                         'Version': `${version}`,
                         'using': "npm"
                     }
