317c7f510ecf07d55d1cf6e27d4dcd921951503f
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-06-15 02:29:16 +0300

    Update discord2.js

--- test/discord2.js
+++ test/discord2.js
@@ -1,1 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL"],
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+  ]
+});
+const chatbot = require("smart-chatbot"); 
+const chatclient = new chatbot.Client({ secretkey: "SECRET KEY", botname: "BOT NAME", ownername: "OWNER NAME" });
 
+client.on("message", async(message) => {
+  if(!message || !message.content || !message.author || message.author.bot) return;
+  
+  if (message.channel.id == "CHANNEL ID") {
+       let soru = message.content;
+
+       let reply = await chatclient.chat({ 
+          message: soru, 
+          user: message.author.id,
+          language: "tr" // (tr, en)
+        });
+     message.channel.send(reply);
+  }
+});
+
+client.login("SECRET DISCORD BOT TOKEN");
