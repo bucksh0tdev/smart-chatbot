b60fe73ad706aead7d5cff9040928945bb60b9a8
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-09-24 17:55:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const baseURL = "https://api.csycraft.xyz";
+const baseURL = "https://api.csycraft.com";
 const version = require('./package.json').version;
 const packagename = require('./package.json').name;
 
@@ -20,12 +20,20 @@ class Client {
                 if(!mainjson.secretkey || typeof mainjson.secretkey !== "string") return console.error("[" + packagename + "] Secret Key Problem");
                 if(!mainjson.botname || typeof mainjson.secretkey !== "string") return console.error("[" + packagename + "] Bot Name Problem");
                 if(!mainjson.ownername || typeof mainjson.secretkey !== "string") return console.error("[" + packagename + "] Owner Name Problem");
+                if(mainjson.gender && (mainjson.gender != "male" && mainjson.gender != "female")) return console.error("[" + packagename + "] Gender Problem");
                 
                 if(!json.message || typeof json.message !== "string") reject("No message was provided");
                 if(!json.user || typeof json.user !== "number") json.user = 1;
                 if(!json.language || typeof json.language !== "string") json.language = "en";
 
-                axios.get(`${baseURL}/chatbot?message=${encodeURIComponent(json.message)}&user=${encodeURIComponent(json.user)}&language=${encodeURIComponent(json.language)}&botname=${encodeURIComponent(mainjson.botname)}&ownername=${encodeURIComponent(mainjson.ownername)}`, {
+                var urlcreate = `${baseURL}/chatbot?message=${encodeURIComponent(json.message)}&user=${encodeURIComponent(json.user)}&language=${encodeURIComponent(json.language)}&botname=${encodeURIComponent(mainjson.botname)}&ownername=${encodeURIComponent(mainjson.ownername)}`;
+                
+                if(mainjson.gender) urlcreate += `&gender=${encodeURIComponent(mainjson.gender)}`;
+                if(mainjson.birth) urlcreate += `&birth=${encodeURIComponent(mainjson.birth)}`;
+                if(mainjson.location) urlcreate += `&location=${encodeURIComponent(mainjson.location)}`;
+                if(mainjson.email) urlcreate += `&email=${encodeURIComponent(mainjson.email)}`;
+              
+                axios.get(urlcreate, {
                     headers: {
                         'Authorization': `${mainjson.secretkey}`,
                         'Version': `${version}`,
